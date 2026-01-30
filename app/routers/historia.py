68abe77253d6c00c5f4368e1d345b6f301942da2
Joao Guilherme <86208023+joaoguilherme23@users.noreply.github.com>
2026-01-30 17:17:06 -0300

    remoção de async

--- app/routers/historia.py
+++ app/routers/historia.py
@@ -13,7 +13,7 @@ router = APIRouter(prefix='/api/v1')
 parametro_1 = {'people', 'planets','starships'}
 
 @router.get('/busca', status_code=200, summary="Retorna informaçoes detalhadas de planeta, personagens e espaço-naves  do Star Wars")
-async def lista_informacao(parametro_1:str, parametro_2:str, token=Depends(verifica_token)):
+def lista_informacao(parametro_1:str, parametro_2:str, token=Depends(verifica_token)):
    try:
         if parametro_1 not in {'people', 'planets','starships'}:
             raise HTTPException(status_code=400, detail="Parametros validos : people, planets, starships")
