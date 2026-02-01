2a72e247f7478b629d5ec7e0a3c48dfeb64708a7
joaoguilherme23 <guilhermetenfen2@hotmail.com>
2026-02-01 15:40:28 -0300

    troca de lib requests para httpx, para a corrotina lista_informação
    A

--- app/routers/historia.py
+++ app/routers/historia.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
-import requests, os
+import httpx, os
 from dotenv import load_dotenv
 from app.core.auth import verifica_token
 
@@ -13,14 +13,18 @@ router = APIRouter(prefix='/api/v1')
 parametro_1 = {'people', 'planets','starships'}
 
 @router.get('/busca', status_code=200, summary="Retorna informaçoes detalhadas de planeta, personagens e espaço-naves  do Star Wars")
-def lista_informacao(parametro_1:str, parametro_2:str, token=Depends(verifica_token)):
+async def lista_informacao(parametro_1:str, parametro_2:str, token=Depends(verifica_token)):
+   
    try:
+        
         if parametro_1 not in {'people', 'planets','starships'}:
             raise HTTPException(status_code=400, detail="Parametros validos : people, planets, starships")
         
-        req = requests.get(f"{endereco_api}/{parametro_1}/?search={parametro_2}")
-        dados = req.json()
-        resultado = dados.get("results", [])
+        async with httpx.AsyncClient() as cliente:
+          
+          req =  await cliente.get(f"{endereco_api}/{parametro_1}/?search={parametro_2}")
+          dados = req.json()
+          resultado = dados.get("results", [])
         return resultado
    
    except Exception:
