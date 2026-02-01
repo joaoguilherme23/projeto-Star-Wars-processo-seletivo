7ab566a96707057da4d4a934c8585ea921247e6b
joaoguilherme23 <guilhermetenfen2@hotmail.com>
2026-02-01 17:50:38 -0300

    tratamento de erro adc quando um token nao é informado

--- app/core/auth.py
+++ app/core/auth.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 api_key = os.getenv('API_KEY')
 
-def verifica_token(x_api_key:str = Header(None)):
+def verifica_token(x_api_key:str | None = Header(default=None)):
+    if x_api_key is None:
+        raise HTTPException(status_code=401, detail="Token não informado")
     if  x_api_key != api_key:
         raise HTTPException(status_code=401, detail="Token não autorizado")
